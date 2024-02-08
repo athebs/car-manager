0d893fb6256d74514073ac274c82821f974ab917
athebs <athena.s.tafur@gmail.com>
2024-02-07 19:30:11 -0600

    still working on services

--- car_management.py
+++ car_management.py
@@ -20,12 +20,10 @@ class CarManager:
     # string to see what is going on 
     def __str__(self):
         return f"car id: {self._id}, model: {self._model}, make: {self._make}, year: {self._year}, mileage {self._mileage}"
-   
+    
     def __repr__(self):
         return str(self)
-    
-   
-
+    # getter of make 
     @property
     def make(self): 
         return self._make  
@@ -38,15 +36,15 @@ class CarManager:
     def input_make(self): 
         new_make = input("What is your Make?", )
         self.make = new_make
-    # to add a model 
+    # getter for model of the car 
     @property 
     def model(self):
         return self._model 
-    
+    # setter for model of the car 
     @model.setter
     def model_setter(self, new_model):
         self._model = new_model
-
+    
     def input_model(self):
         new_model = input('What is your Model?', )
         self._model = new_model
@@ -74,6 +72,16 @@ class CarManager:
     def input_mileage(self):
         new_mileage = input('What is your car milage', )
         self._mileage = new_mileage
+    
+    @property
+    def services(self):
+        return self._services
+    
+    @services.setter
+    def services_setter(self, updated_services): 
+        self._services = update_services
+        return updated_services
+    
         
 def display_welcome():
     print("""----  WELCOME  ----
@@ -96,6 +104,7 @@ def add_car():
 #view how many cars are there 
 def view_cars():
     print(CarManager.all_cars)
+
 #view total number of cars 
 def view_total_cars():
     print(f"Total cars present: {CarManager.total_cars}")
@@ -104,15 +113,21 @@ def view_total_cars():
 def view_car_details():
     print(f"Car ID: {self._id}, Model: {self._model}, Make: {self._make}, Year: {self._year}, Mileage {self._mileage}")
 
+# function to find the car ID
+def car_finder():
+    for id_values in CarManager.all_cars:
+        print("hi")#TODO find a meaningfull to loop through 
+
+
 # Car services 
 def update_services():
     user_choice ="""What do you need done? Please Enter a Number Between 1-3 
           if user
             {CarManager.services_offered}"""
     if user_choice == "1":
-
+        CarManager
     if user_choice == "2":
-        view_cars()
+        
     if user_choice == "3":
         view_total_cars()
 
@@ -148,3 +163,5 @@ def choice_welcome():
 # ford = CarManager("ford" , "mustang", 2018, 679, ["change tires", "oil change"])
 # new_car = CarManager()
 print(choice_welcome())
+
+print(car_finder)
